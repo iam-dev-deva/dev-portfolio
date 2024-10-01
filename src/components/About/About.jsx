154392e2e6d5848763f71eaa464a505a25b4f147
Devaraj <deva.arcee@gmail.com>
2024-10-02 00:18:22 +0530

    about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,11 +11,11 @@ const About = () => {
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa qui, consequuntur eius dolorem dolores animi. Natus similique,
           fugit ullam quaerat at maiores id minima neque totam nam doloremque quia incidunt.</p>
       </div>
-      <div className="bio-sector">
+      <div className="about">
         <div className="bio-image">
           <img src={profile_img} alt="" />
         </div>
-        <div className="bio-data">
+        <div className="content">
           <div className='bio-data-heading'>
             <h2>Backend & Web Developer</h2>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa qui, consequuntur</p>
@@ -23,16 +23,16 @@ const About = () => {
           <div className="bio-data-rows">
             <div className="row">
               <ul className="row-data">
-                <li><BiChevronRight/><b>BirthDay: </b> 1 April 2000</li>
-                <li><BiChevronRight/><b>Website:</b> www.example.com</li>
-                <li><BiChevronRight/><b>Phone:</b> 1234567890</li>
+                <li><BiChevronRight/><b>BirthDay: </b> <span>1 April 2000</span></li>
+                <li><BiChevronRight/><b>Website:</b><span> www.example.com</span></li>
+                <li><BiChevronRight/><b>Phone:</b><span> 1234567890</span></li>
               </ul>
             </div>
             <div className="row">
               <ul className="row-data">
-                <li><BiChevronRight/><b>Age: </b> 20</li>
-                <li><BiChevronRight/><b>Degree:</b> Karur</li>
-                <li><BiChevronRight/><b>Email:</b> example@example.com</li>
+                <li><BiChevronRight/><b>Age: </b> <span>20</span></li>
+                <li><BiChevronRight/><b>Degree:</b><span> Karur</span></li>
+                <li><BiChevronRight/><b>Email:</b> <span>example@example.com</span></li>
               </ul>
             </div>
           </div>
