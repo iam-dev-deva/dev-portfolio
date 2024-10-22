05f012d5ba0d50eda23917814fcb21f80b79ca17
Deva-CN <devaraj@codenatives.com>
2024-10-22 19:43:21 +0530

    resume fix

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -29,16 +29,16 @@ const Resume = () => {
             <h2>Education</h2>
             <div className="column-data">
               <h3 className="column-title">MCA</h3>
-              <h4>2022 - 2024</h4>
-              <span>
+              <h4 className='column-sub'>2022 - 2024</h4>
+              <span className='column-sub'>
                 arts and sciend collage
               </span><br />
               <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus aspernatur accusantium, eaque labore consequatur minima necessitatibus? Minima, a libero modi deleniti, ipsa, consectetur esse voluptate corporis velit officiis fugit quos?</span>
             </div>
             <div className="column-data">
               <h3 className="column-title">BCA</h3>
-              <h4>2019 - 2022</h4>
-              <span>
+              <h4 className='column-sub'> 2019 - 2022</h4>
+              <span className='column-sub'>
                 arts and sciend collage
               </span><br />
               <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis, in earum, eos perferendis sint, beatae reprehenderit ullam alias inventore quibusdam recusandae sed? Facere necessitatibus repellendus vero doloremque, assumenda odio molestiae.</span>
@@ -51,8 +51,8 @@ const Resume = () => {
             <h2 >Professional Experience</h2>
             <div className="column-data">
               <h2 className="column-title">Backend Developer</h2>
-              <span>2022-present</span><br />
-              <span>karur</span>
+              <span className='column-sub'>2022-present</span><br />
+              <span className='column-sub'>karur</span>
               <ul>
                 <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
                 <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project.</li>
