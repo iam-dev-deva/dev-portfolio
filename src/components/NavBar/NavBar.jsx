21e9a823c03afbfd770b5f3e81bc782e40dbe30b
Deva-CN <devaraj@codenatives.com>
2024-10-28 17:04:28 +0530

    hamburger menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './NavBar.css'
 import logo from '../../assets/logo.png'
 import {NavLink } from 'react-router-dom';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const NavBar = () => {
 
@@ -12,18 +13,23 @@ const NavBar = () => {
       window.scrollY > 50 ? setSticky(true) : setSticky(false);
     })
   },[])
-
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  }
   return (
     <nav className={`container  ${sticky? `dark-nav` : ''}`} >
         <img className='logo' src={logo} alt="" />
         {/* <span className='logo'>Portfolio</span> */}
-        <ul>
+        <ul className={mobileMenu?'':'hide-mobile-menu'}>
             <li className="nav-item"> <NavLink activeclassname="active" to='/'>Home</NavLink ></li>
             <li className='nav-item'><NavLink activeclassname="active" to='/about' >About</NavLink ></li>
             <li className='nav-item'><NavLink activeclassname="active" to='/resume'>Resume</NavLink ></li>
             <li className='nav-item'><NavLink activeclassname="active" to='/project'>Projects</NavLink ></li>
-            <li><button className='btn'><NavLink  to='/contact'>Contact</NavLink ></button></li>
+            <li className={mobileMenu?'nav-item':'btn'}><NavLink  to='/contact'>Contact</NavLink ></li>
         </ul>
+      <GiHamburgerMenu  className='menu-icon' onClick={toggleMenu} />
+
     </nav>
   )
 }
