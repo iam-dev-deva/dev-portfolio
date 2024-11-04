a4f6f6888622ee1303933a8d725f7a7c7aa9faa6
Deva-CN <devaraj@codenatives.com>
2024-11-04 16:44:40 +0530

    responsive fixs

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -54,6 +54,7 @@ const About = () => {
 
   return (
     <div className="container">
+      {/* <div className="container"> */}
       <div className="top-content" data-aos="fade-in" data-aos-duration="1000">
         <h1>About</h1>
         <p>
@@ -61,134 +62,139 @@ const About = () => {
           consequuntur eius dolorem dolores animi. Natus similique, fugit ullam
           quaerat at maiores id minima neque totam nam doloremque quia incidunt.
         </p>
+      {/* </div> */}
       </div>
 
       <section className="bio-data-section">
-        <div className="about" data-aos="fade-up" data-aos-duration="1000">
-          <div className="bio-image">
-            <img src={profile_img} alt="" />
-          </div>
-          <div className="content">
-            <div className="bio-data-heading">
-              <h2>Backend & Web Developer</h2>
-              <p className="py-3">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
+        <div className="container">
+          <div className="about" data-aos="fade-up" data-aos-duration="1000">
+            <div className="bio-image">
+              <img src={profile_img} alt="" />
             </div>
-            <div className="bio-data-rows">
-              <div className="row">
-                <ul className="row-data">
-                  <li>
-                    <BiChevronRight />
-                    <b>BirthDay: </b>
-                    <span>1 April 2000</span>
-                  </li>
-                  <li>
-                    <BiChevronRight />
-                    <b>Website:</b>
-                    <span> deva.arcee@gmail.com</span>
-                  </li>
-                  <li>
-                    <BiChevronRight />
-                    <b>Phone:</b>
-                    <span> 9858500932</span>
-                  </li>
-                </ul>
+            <div className="content">
+              <div className="bio-data-heading">
+                <h2>Backend & Web Developer</h2>
+                <p className="py-3">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
               </div>
-              <div className="row">
-                <ul className="row-data">
-                  <li>
-                    <BiChevronRight />
-                    <b>Age: </b> <span>20</span>
-                  </li>
-                  <li>
-                    <BiChevronRight />
-                    <b>Degree:</b>
-                    <span> MCA</span>
-                  </li>
-                  <li>
-                    <BiChevronRight />
-                    <b>Email:</b> <span>example@example.com</span>
-                  </li>
-                </ul>
+              <div className="bio-data-rows">
+                <div className="row">
+                  <ul className="row-data">
+                    <li>
+                      <BiChevronRight />
+                      <b>BirthDay: </b>
+                      <span>1 April 2000</span>
+                    </li>
+                    <li>
+                      <BiChevronRight />
+                      <b>Website:</b>
+                      <span> deva.arcee@gmail.com</span>
+                    </li>
+                    <li>
+                      <BiChevronRight />
+                      <b>Phone:</b>
+                      <span> 9858500932</span>
+                    </li>
+                  </ul>
+                </div>
+                <div className="row">
+                  <ul className="row-data">
+                    <li>
+                      <BiChevronRight />
+                      <b>Age: </b> <span>20</span>
+                    </li>
+                    <li>
+                      <BiChevronRight />
+                      <b>Degree:</b>
+                      <span> MCA</span>
+                    </li>
+                    <li>
+                      <BiChevronRight />
+                      <b>Email:</b> <span>example@example.com</span>
+                    </li>
+                  </ul>
+                </div>
+                <p className="py-3">
+                  Officiis eligendi itaque labore et dolorum mollitia officiis
+                  optio vero. Quisquam sunt adipisci omnis et ut. Nulla
+                  accusantium dolor incidunt officia tempore.Et eius omnis.
+                  Cupiditate ut dicta maxime officiis quidem quia. Sed et
+                  consectetur qui quia repellendus itaque neque.
+                </p>
               </div>
-              <p className="py-3">
-                Officiis eligendi itaque labore et dolorum mollitia officiis
-                optio vero. Quisquam sunt adipisci omnis et ut. Nulla
-                accusantium dolor incidunt officia tempore.Et eius omnis.
-                Cupiditate ut dicta maxime officiis quidem quia. Sed et
-                consectetur qui quia repellendus itaque neque.
-              </p>
             </div>
           </div>
         </div>
       </section>
 
       <section className="stats-section">
-        <div className="stats" data-aos="fade-up" data-aos-duration="1200">
-          <div className="stat">
-            <span className="stat-icon">
-              <GoProject />
-            </span>
-            <div className="item">
-              <h2>
-                <CountUpWrapper
-                  start={0}
-                  end={5}
-                  delay={0}
-                  enableScrollSpy={true}
-                />
-              </h2>
-              <span>Projects Done</span>
+        <div className="container">
+          <div className="stats" data-aos="fade-up" data-aos-duration="1200">
+            <div className="stat">
+              <span className="stat-icon">
+                <GoProject />
+              </span>
+              <div className="item">
+                <h2>
+                  <CountUpWrapper
+                    start={0}
+                    end={5}
+                    delay={0}
+                    enableScrollSpy={true}
+                  />
+                </h2>
+                <span>Projects Done</span>
+              </div>
             </div>
-          </div>
-          <div className="stat">
-            <span className="stat-icon">
-              <BiFile />
-            </span>
-            <div className="item">
-              <h2>
-                <CountUpWrapper
-                  start={0}
-                  end={220}
-                  delay={0}
-                  enableScrollSpy={true}
-                />
-              </h2>
-              <span>Files Committed</span>
+            <div className="stat">
+              <span className="stat-icon">
+                <BiFile />
+              </span>
+              <div className="item">
+                <h2>
+                  <CountUpWrapper
+                    start={0}
+                    end={220}
+                    delay={0}
+                    enableScrollSpy={true}
+                  />
+                </h2>
+                <span>Files Committed</span>
+              </div>
             </div>
-          </div>
-          <div className="stat">
-            <span className="stat-icon">
-              <GiSuitcase />
-            </span>
-            <div className="item">
-              <h2>
-                <CountUpWrapper
-                  start={0}
-                  end={2}
-                  delay={0}
-                  enableScrollSpy={true}
-                />
-              </h2>
-              <span>Years of Experience</span>
+            <div className="stat">
+              <span className="stat-icon">
+                <GiSuitcase />
+              </span>
+              <div className="item">
+                <h2>
+                  <CountUpWrapper
+                    start={0}
+                    end={2}
+                    delay={0}
+                    enableScrollSpy={true}
+                  />
+                </h2>
+                <span>Years of Experience</span>
+              </div>
             </div>
-          </div>
-          <div className="stat">
-            <span className="stat-icon">
-              <CgCode />
-            </span>
-            <div className="item">
-              <h2>
-                <CountUpWrapper
-                  start={0}
-                  end={10}
-                  delay={0}
-                  enableScrollSpy={true}
-                />
-              </h2>
-              <span>Language Worked</span>
+            <div className="stat">
+              <span className="stat-icon">
+                <CgCode />
+              </span>
+              <div className="item">
+                <h2>
+                  <CountUpWrapper
+                    start={0}
+                    end={10}
+                    delay={0}
+                    enableScrollSpy={true}
+                  />
+                </h2>
+                <span>Technical Skills</span>
+              </div>
             </div>
           </div>
         </div>
